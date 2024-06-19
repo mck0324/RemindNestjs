4b93c5b7cfc191b8a480bdcea72dda34c8d60ae6
mck0324 <dch3030@naver.com>
2024-06-19 15:35:19 +0900

    Service Single-responsibility priciple을 이용하여 만들기/MovieEntity 만들기

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,22 +1,29 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 import { retry } from 'rxjs';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
 
+    constructor(private readonly movieService: MoviesService) {}
+
     @Get()
     getAll(){
-        return "This will return all movies";
+        return this.movieService.getAll();
+    }
+    @Get('search')
+    search(@Query('year') searchingYear: string) {
+        return `We are searching for a movie made after: ${searchingYear}`
     }
 
     @Get("/:id")
     getOne(@Param("id") id: string) {
-        return `This will return one Movie with the id: ${id}`;
+        return this.movieService.getOne(id);
     } 
 
     @Post()
-    create() {
-        return "This will create a movie";
+    create(@Body() movieData) {
+        return movieData ;
     }
     @Delete("/:id")
     remove(@Param("id") movieId: string) {
@@ -27,3 +34,4 @@ export class MoviesController {
         return `This will patch a movie with the id: ${movieId}`;
     }
 }
+ 
